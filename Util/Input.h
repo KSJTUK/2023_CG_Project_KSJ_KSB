111f5c62efa4f842fe1b93289c44e28fd2d4bc4d
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-12-07 02:06:00 +0900

    Input파일 생성 및 커밋시 lib파일 포함하도록 변경

---
+++ Util/Input.h
@@ -0,0 +1,56 @@
+#pragma once
+
+enum class KEY_STATE {
+	/// <summary>
+	///  nothing state 
+	/// </summary>
+	NONE,
+
+	/// <summary>
+	///  pressed on time 
+	/// </summary>
+	DOWN,
+
+	/// <summary>
+	///  pressing during time 
+	/// </summary>
+	PRESS,
+
+	/// <summary>
+	///  released on time  
+	/// </summary>
+	RELEASE
+};
+
+class Input {
+private:
+	Input();
+	~Input();
+
+	Input(const Input& other) = delete;
+	Input& operator=(const Input& other) = delete;
+
+private:
+	static Input* m_instance;
+
+public:
+	static Input* GetInstance();
+
+private:
+	GLFWwindow* m_window{ nullptr };
+
+	KEY_STATE* m_keyboardState{ nullptr };
+
+	float2 m_prevMouse{ };
+	float2 m_deltaMouse{ };
+
+public:
+	const KEY_STATE GetKey(int key) const;
+	const float2 GetDeltaMouse() const { return m_deltaMouse; };
+
+public:
+	void Init(GLFWwindow* window);
+	void Update();
+
+};
+
