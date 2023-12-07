1b739473ee79aa092f4af8032a40b9c7a58297e4
wewe231223@Laptop <wewe231223@kpu.ac.kr>
2023-12-07 22:15:36 +0900

    Camera Movenet

--- Graphics/Camera.cpp
+++ Graphics/Camera.cpp
@@ -1,9 +1,7 @@
 #include "pch.h"
 #include "Camera.h"
-
-
 #include "Graphics/Shader.h"
-
+#include "Util/Input.h"
 
 Camera::Camera(GLFWwindow* window, glm::vec3 EYE, glm::vec3 AT) : m_window(window), m_eye(EYE), m_at(AT){
 	int width, height;
@@ -38,13 +36,66 @@ void FreeCamera::Render(){
 
 
 }
+constexpr auto CameraSpeed = 20.f;
+constexpr auto MOUSE_SENSITIVE = 0.1f;
+
 
 void FreeCamera::Update(float DeltaTime){
 	int width, height;
 	glfwGetFramebufferSize(m_window, &width, &height);
 	m_aspect = static_cast<float>(width) / static_cast<float>(height);
 
+	if (Input::GetInstance()->GetKey(GLFW_KEY_W) == KEY_STATE::PRESS) {
+		m_eye -= m_basisZ * DeltaTime * CameraSpeed;
+	}
 
+
+	if (Input::GetInstance()->GetKey(GLFW_KEY_S) == KEY_STATE::PRESS) {
+		m_eye += m_basisZ * DeltaTime * CameraSpeed;
+	}
+
+
+	if (Input::GetInstance()->GetKey(GLFW_KEY_A) == KEY_STATE::PRESS) {
+		m_eye -= m_basisX * DeltaTime * CameraSpeed;
+	}
+
+
+	if (Input::GetInstance()->GetKey(GLFW_KEY_D) == KEY_STATE::PRESS) {
+		m_eye += m_basisX * DeltaTime * CameraSpeed;
+	}
+
+	if (Input::GetInstance()->GetKey(GLFW_KEY_Q) == KEY_STATE::PRESS) {
+		m_eye -= m_basisY * DeltaTime * CameraSpeed;
+
+	}
+
+
+	if (Input::GetInstance()->GetKey(GLFW_KEY_E) == KEY_STATE::PRESS) {
+		m_eye += m_basisY * DeltaTime * CameraSpeed;
+
+	}
+
+
+	float2 deltaMouse = Input::GetInstance()->GetDeltaMouse();
+
+	if (!(deltaMouse.x == 0 and deltaMouse.y == 0)) {
+
+		glm::mat4 cameraRotate{ 1.f };
+
+
+		cameraRotate = glm::rotate(cameraRotate, glm::radians(-deltaMouse.y * MOUSE_SENSITIVE), m_basisX);
+		cameraRotate = glm::rotate(cameraRotate, glm::radians(-deltaMouse.x * MOUSE_SENSITIVE), m_basisY);
+
+		m_at = glm::normalize(glm::vec3(cameraRotate * glm::vec4(m_at, 1.f)));
+
+
+		m_basisZ = glm::normalize(-m_at);
+		m_basisX = glm::normalize(glm::cross(m_up, m_basisZ));
+		m_basisY = glm::cross(m_basisZ, m_basisX);
+
+
+
+	}
 
 
 }
--- Graphics/Renderer.h
+++ Graphics/Renderer.h
@@ -1,6 +1,4 @@
 #pragma once
-
-
 class Renderer {
 public:
 	Renderer();
